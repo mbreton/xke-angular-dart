d1c32cbc04552776d87f9d7bc01f348b7c9f4c1d
RNiveau <romain.niveau@gmail.com>
2014-05-04 12:21:17 +0200

    Solution

--- lib/workshop/detail_controller.dart
+++ lib/workshop/detail_controller.dart
@@ -14,7 +14,15 @@ class DetailController {
   Log log;
   
   DetailController(Http _http, RouteProvider _routeProvider) {
-    
+    Future.wait([_http.get("apache-log.json").then((HttpResponse httpResponse) {
+    	for (Map mLog in httpResponse.data) {
+        	Log l = new Log.fromJsonMap(mLog);
+            if (l.id == _routeProvider.parameters['detailId']) {
+              log = l;
+              break;
+            }
+        }
+    })]);
   }
   
 }
